0e2439e5c28c243a54b2cd1c3963069807531b50
Blagneaux <bastien.lagneaux@ens-rennes.fr>
2023-01-03 14:27:25 +0100

    clean code vonKarmann

--- vonKarmann.jl
+++ vonKarmann.jl
@@ -15,31 +15,6 @@ cycle = range(0, 8, length=24*8)
 sim_step!(swimmer, 150, remeasure=true)
 sim_time(swimmer)
 
-# plot the vorcity ω=curl(u) scaled by the body length L and flow speed U
-function plot_vorticity(sim)
-	@inside sim.flow.σ[I] = WaterLily.curl(3, I, sim.flow.u) * sim.L / sim.U
-	contourf(sim.flow.σ',
-			 color=palette(:BuGn), clims=(-5, 5), linewidth=0,
-			 aspect_ratio=:equal, legend=true, border=:none)
-
-	# contourf(sim.flow.u[:,:,2]', color=cgrad(:roma, rev=true),
-	# 		 clims=(-1, 1), linewidth=0,
-	# 		 aspect_ratio=:equal, legend=true, border=:none)
-
-
-	# contourf(sim.flow.p',
-	# 		 clims=(-1, 1), linewidth=0,
-	# 		 aspect_ratio=:equal, legend=true, border=:none)
-end
-
-
-print(sim_time(swimmer), "\n")
-# make a gif over a swimming cycle
-@gif for t ∈ sim_time(swimmer) .+ cycle
-    print(t,"\n")
-	sim_step!(swimmer, t, remeasure=true, verbose=true)
-	plot_vorticity(swimmer)
-end
 
 function get_force(sim, t)
 	sim_step!(sim, t, remeasure=true)
