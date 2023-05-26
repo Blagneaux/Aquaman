f0630d596729a5c41ae91ed304e99e1dd44d32c2
Blagneaux <bastien.lagneaux@ens-rennes.fr>
2023-05-26 09:45:41 +0200

    waterlily no more

---
+++ WaterLily/plotSdf.jl
@@ -0,0 +1,83 @@
+using WaterLily, StaticArrays, PlutoUI, Interpolations, Plots, Images
+using LinearAlgebra: norm2
+
+# include("capsule.jl")
+include("cambridgeFish.jl")
+
+fit = y -> scale(
+    interpolate(y, BSpline(Quadratic(Line(OnGrid())))),
+    range(0,1,length=length(y))
+)
+
+width = [0.02, 0.06, 0.06, 0.05, 0.03, 0.015, 0.01]
+thk = fit(width)
+
+_nthread = Threads.nthreads()
+if _nthread==1
+    @warn "WaterLily.jl is running on a single thread.\n
+Launch Julia with multiple threads to enable multithreaded capabilities:\n
+    \$julia -t auto $PROGRAM_FILE"
+else
+    print("WaterLily.jl is running on ", _nthread, " thread(s)\n")
+end
+
+function wall(a, b)
+    function sdf(x,t)
+        xa = x-a
+        ba = b-a
+        h = clamp(dot(xa,ba)/dot(ba,ba), 0.0, 1.0)
+        return norm2(xa - ba*h) - 10
+    end
+
+    function map(x,t)
+        xc = x - [5L, 258/2]
+        return xc
+    end
+
+    return AutoBody(sdf, map)
+end
+
+L,A,St,U = 71.2,0.466,0.61,0.89
+# capsuleShape = capsule(L, St, A, U; n, m)
+fishShape = fish(thk; L=81.8, Re=6070, n, m)
+
+wallShape1 = wall([-600,110], [400,110])
+wallShape2 = wall([-600,-110], [400,-110])
+
+# swimmerBody = capsuleShape + wallShape1 + wallShape2
+swimmerBodyCambridge = fishShape + wallShape1 + wallShape2
+
+swimmer = Simulation((642,258), [0.,0.], L, U=0.89; ν=U*L/6070, body=swimmerBodyCambridge)
+
+
+# Save a time span for one swimming cycle
+period = 2A/St
+cycle = range(0, 23*period/24, length=24)
+
+foreach(rm, readdir("C:/Users/blagn771/Desktop/PseudoGif", join=true))
+
+function computeSDF(sim, t)
+    s = copy(sim.flow.p)
+    for I ∈ inside(s)
+        x = loc(0, I)
+        s[I] = sim.body.sdf(x,t*sim.L/sim.U)::Float64
+    end
+
+    contourf(s', clims=(-L,2L), linewidth=0,
+            aspect_ratio=:equal, legend=true, border=:none)
+    savefig("C:/Users/blagn771/Desktop/PseudoGif/frame"*string(t)*".png")
+end
+
+@time @gif for t ∈ sim_time(swimmer) .+ cycle
+    sim_step!(swimmer, t, remeasure=true, verbose=true)
+    computeSDF(swimmer, t)
+end
+
+# @gif for t ∈ cycle
+# 	measure!(swimmer, t*swimmer.L/swimmer.U)
+# 	contour(swimmer.flow.μ₀[:,:,1]',
+# 			aspect_ratio=:equal, legend=true, border=:none)
+# end
+
+#225.172887 seconds (1.59 G allocations: 59.436 GiB, 3.35% gc time, 0.46% compilation time)
+#1764.266766 seconds (15.36 G allocations: 513.906 GiB, 3.79% gc time, 0.10% compilation time)
---
+++ WaterLily/shapeCatfish.jl
@@ -0,0 +1,86 @@
+using WaterLily, StaticArrays, PlutoUI, Interpolations, Plots, Images
+using LinearAlgebra: norm2
+
+fit = y -> scale(
+    interpolate(y, BSpline(Quadratic(Line(OnGrid())))),
+    range(0,1,length=length(y))
+)
+
+url2 = "C://Users//blagn771//Desktop//jl_pCJyN8xGle.jpg"
+catfish = load(url2)
+plot(catfish)
+
+nose, len = (45,47.5), 150
+width = [0.02, 0.06, 0.06, 0.055, 0.04, 0.025, 0.01]
+scatter!(
+    nose[1] .+ len .* range(0,1,length=length(width)),
+    nose[2] .- len .* width, color= :blue, legend=false
+)
+thk = fit(width)
+x = 0:0.01:1
+plot!(
+    nose[1] .+ len .* x,
+    [nose[2] .- len .* thk.(x), nose[2] .+ len .* thk.(x)],
+    color=:red
+)
+
+# envelope = [0.051, 0.022, 0.0435, 0.0801, 0.133, 0.2225]./0.2225 # % of the tail amplitude for a catfish swimming at 1 BL/s (Anguilliform Locomotion across a Natural Range of Swimming Speeds)
+# amp = fit(envelope)
+
+# """λ = 0.71
+# scatter(0:0.2:1, envelope)
+# colors = palette(:cyclic_wrwbw_40_90_c42_n256)
+# for t in 1/12:1/12:1
+# 	plot!(x, amp.(x) .* sin.(2π/λ * x .- 2π*t),
+# 		  color=colors[floor(Int,t*256)])
+# end
+# plot!(ylim=(-1.4,1.4), legend=false)"""
+
+# function fish(thk, amp, k=2*π/0.71; L=2^6, A=0.1, St=0.3, Re=5430)
+# 	# fraction along fish length
+# 	s(x) = clamp(x[1]/L, 0, 1)
+
+# 	# fish geometry: thickened line SDF
+# 	sdf(x,t) = √sum(abs2, x - L * SVector(s(x), 0.)) - L * thk(s(x))
+
+
+# 	# fish motion: travelling wave
+# 	ω = 2π * St * U/(2A * L)
+# 	function map(x, t)
+# 		xc = x - [L, L] # shift origin
+# 		return xc - SVector(0., A * L * amp(s(xc)) * sin(k*s(xc)-ω*t))
+# 	end
+
+# 	# make the fish simulation
+# 	return Simulation((3L+2,2L+2), [U,0.], L;
+# 						ν=U*L/Re, body=AutoBody(sdf,map))
+# end
+
+# # Create the swimming catfish
+# L,A,St = 3*2^5,0.2225,0.63
+# U = 1
+# λ = 0.71
+# swimmer = fish(thk, amp, 2*π/λ; L, A, St);
+
+# # Save a time span for one swimming cycle
+# period = 2A/St
+# cycle = range(0, 23/24*period, length=24)
+
+# @gif for t ∈ cycle
+# 	measure!(swimmer, t*swimmer.L/swimmer.U)
+# 	contour(swimmer.flow.μ₀[:,:,1]',
+# 			aspect_ratio=:equal, legend=false, border=:none)
+# end
+
+# function plot_pressure(sim)
+# 	contourf(sim.flow.p',
+# 			 clims=(-1, 1), linewidth=0,
+# 			 aspect_ratio=:equal, legend=true, border=:none)
+# end
+
+# # make a gif over a swimming cycle
+# @gif for t ∈ sim_time(swimmer) .+ cycle
+# 	sim_step!(swimmer, t, remeasure=true, verbose=false)
+# 	# plot_vorticity(swimmer)
+# 	plot_pressure(swimmer)
+# end
