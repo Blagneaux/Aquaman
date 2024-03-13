0c85b574ea10c9f142ed054e6f54c8881a40e651
Blagneaux <bastien.lagneaux@ens-rennes.fr>
2024-03-13 10:40:03 +0100

    siming dataset

--- python/dataAugmentation.py
+++ python/dataAugmentation.py
@@ -9,7 +9,54 @@ import os
 
 # -------------------------------------------------------------------------------------------
 
-model = YOLO("C:/Users/blagn771/Desktop/FishDataset/segment/train1280_32_291/weights/best.pt")
+def crop_and_resize_image_from_mask(input_image_path, file, target_size=(640, 640)):
+    # Find the corresponding mask
+    print(file)
+    input_mask_path = input_image_path[:-(len(file)+1+6)]+"masks/"+file
+
+    # Read the input image
+    img = cv2.imread(input_image_path)
+    mask = cv2.imread(input_mask_path)
+
+    # Get the dimensions of the input image
+    img_height, img_width = img.shape[:2]
+
+    # Get the contours in the image and select the biggest object
+    min_area = 1500
+    masks_gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    contours, _ =cv2.findContours(masks_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    valid_contours = [contour for contour in contours if cv2.contourArea(contour) >= min_area]
+    if valid_contours:
+        contour_coordinates = valid_contours[0]
+        # Calculate the center of the contour
+        x_mean = np.int16(np.mean([elmt[0][0] for elmt in contour_coordinates]))
+        y_mean = np.int16(np.mean([elmt[0][1] for elmt in contour_coordinates]))
+
+        print(x_mean, y_mean)
+
+        # Calculate the cropping coordinates
+        crop_xmin = max(0, x_mean - target_size[0] // 2)
+        crop_ymin = max(0, y_mean - target_size[1] // 2)
+        crop_xmax = min(img_width, x_mean + target_size[0] // 2)
+        crop_ymax = min(img_height, y_mean + target_size[1] // 2)
+
+        # Check if the target region exceeds image boundaries
+        if (crop_xmax - crop_xmin) < target_size[0]:
+            if x_mean - (target_size[0] // 2) < 0:
+                crop_xmax = crop_xmin + target_size[0]
+            elif x_mean + (target_size[0] // 2) > img_width:
+                crop_xmin = crop_xmax - target_size[0]
+
+        if (crop_ymax - crop_ymin) < target_size[1]:
+            if y_mean - (target_size[1] // 2) < 0:
+                crop_ymax = crop_ymin + target_size[1]
+            elif y_mean + (target_size[1] // 2) > img_height:
+                crop_ymin = crop_ymax - target_size[1]
+
+        # Crop and resize the image
+        cropped_resized_img = img[crop_ymin:crop_ymax, crop_xmin:crop_xmax]
+        cv2.imwrite(input_image_path[:-4]+"-1.png", cropped_resized_img)
+
 
 def crop_and_resize_image(input_image_path, target_size=(640, 640)):
     # Read the input image
@@ -64,9 +111,14 @@ def crop_and_resize_image(input_image_path, target_size=(640, 640)):
         cv2.imwrite(input_image_path[:-4]+'-4.png', cropped_resized_img)
 
 
-input_folder = "C:/Users/blagn771/Desktop/FishDataset/Fish1C1/images"
+input_folder = "C:/Users/blagn771/Desktop/simingImages/20240116_f68_crop1/images"
+fromMask = True
 for file in os.listdir(input_folder):
     file_path = os.path.join(input_folder, file)
     if file_path[-6] != '-':
-        crop_and_resize_image(file_path, (640, 640))
-        crop_and_resize_image(file_path, (1200, 1200))
+        if fromMask:
+            crop_and_resize_image_from_mask(file_path, file, (320, 320))
+        else:
+            model = YOLO("C:/Users/blagn771/Desktop/FishDataset/segment/train1280_32_291/weights/best.pt")
+            crop_and_resize_image(file_path, (640, 640))
+            # crop_and_resize_image(file_path, (1200, 1200))
--- python/ezSegmentation.py
+++ python/ezSegmentation.py
@@ -8,7 +8,7 @@ import numpy as np
 # and create a mask and an image for every frame of the video
 
 frameSeg = False
-videoSeg = False
+videoSeg = True
 videoProcess = True
 
 if frameSeg:
@@ -32,9 +32,10 @@ if frameSeg:
 
 if videoSeg:
     print("videoSeg")
-    cap = cv2.VideoCapture("C:/Users/blagn771/Desktop/T3_Fish3_C1_270923 - Trim0.mp4")
+    cap = cv2.VideoCapture("C:/Users/blagn771/Desktop/20240116_f68_crop1.mp4")
     frames = []
-    while cap.isOpened() and len(frames) < 200:
+    while cap.isOpened() and len(frames) < 59:
+        print(len(frames))
         ret, frame = cap.read()
 
         if ret:
@@ -42,7 +43,7 @@ if videoSeg:
 
     medianFrame = np.median(frames, axis=0).astype(dtype=np.uint8)
 
-    cv2.imwrite("medianT3_Fish3_C1_270923.png", medianFrame)
+    cv2.imwrite("simingRaw.png", medianFrame)
     cv2.imshow('medianPicture', medianFrame)
     cv2.waitKey(0)
     
@@ -51,10 +52,10 @@ if videoSeg:
 
 if videoProcess:
     print("videoProcess")
-    grayMedian = cv2.imread("C:/Users/blagn771/Documents/Aquaman/Aquaman/medianT3_Fish3_C1_270923.png")
+    grayMedian = cv2.imread("C:/Users/blagn771/Documents/Aquaman/Aquaman/simingRaw.png")
     grayMedian = cv2.cvtColor(grayMedian, cv2.COLOR_BGR2GRAY)
 
-    cap = cv2.VideoCapture("C:/Users/blagn771/Desktop/T3_Fish3_C1_270923 - Trim1.mp4")
+    cap = cv2.VideoCapture("C:/Users/blagn771/Desktop/20240116_f68_crop1.mp4")
     count = 1
     while cap.isOpened():
         ret, frame = cap.read()
@@ -64,8 +65,8 @@ if videoProcess:
             dframe = cv2.absdiff(gframe, grayMedian)
             th, dframe = cv2.threshold(dframe, 30, 255, cv2.THRESH_BINARY)
 
-            cv2.imwrite("C:/Users/blagn771/Desktop/Fish3C1/images"+"/Fish1C1_frame-"+str(10000+count)[1:]+".png", frame)
-            cv2.imwrite("C:/Users/blagn771/Desktop/Fish3C1/masks"+"/Fish1C1_frame-"+str(10000+count)[1:]+".png", dframe)
+            cv2.imwrite("C:/Users/blagn771/Desktop/simingImages/20240116_f68_crop1/images"+"/20240116_f68_crop1_frame-"+str(10000+count)[1:]+".png", frame)
+            cv2.imwrite("C:/Users/blagn771/Desktop/simingImages/20240116_f68_crop1/masks"+"/20240116_f68_crop1_frame-"+str(10000+count)[1:]+".png", dframe)
             count+=1
             cv2.imshow('frame', dframe)
             if cv2.waitKey(10) & 0xFF == ord("q"):
